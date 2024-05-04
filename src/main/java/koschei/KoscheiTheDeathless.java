ada3a4edb0970f067fcebfd507c2fb89f7ad164e
daniil <you@example.com>
2024-05-04 13:20:56 +0400

    completed

--- src/main/java/koschei/KoscheiTheDeathless.java
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -1,5 +1,7 @@
 package koschei;
 
+import koschei.models.Duck5;
+import koschei.models.Egg6;
 import koschei.models.Ocean1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -7,14 +9,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class KoscheiTheDeathless {
 
+
     private Ocean1 ocean;
+    private Egg6 egg;
 
     public String getRulesByDeth() {
-        return "На свете есть океан , " + ocean.toString();
+        return "На свете есть океан , " + ocean.toString() + " " + egg.toString();
     }
 
     @Autowired
     public void setOcean(Ocean1 ocean) {
         this.ocean = ocean;
+    }
+
+    @Autowired
+    public void setEgg(Egg6 egg) {
+        this.egg = egg;
     }
 }
